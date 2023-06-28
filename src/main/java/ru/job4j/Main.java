16773f82bc9708103038fc98260fe69ecfb76ba2
Bibarys <bibaryssultanbek2002@gmail.com>
2023-06-28 16:41:36 +0600

    3. Арифметические операции. [#185  [#185 #411754]]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,9 +1,10 @@
 package ru.job4j;
 
 public class Main {
+    @SuppressWarnings({"checkstyle:ParenPad", "checkstyle:EmptyLineSeparator"})
     public static void main(String[] args) {
 
-        System.out.println("биба world");
+        System.out.println("биба wмвамав");
 
     }
 }
