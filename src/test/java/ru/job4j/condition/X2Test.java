0517edc1436b2f41b414c0318fed1987f0622403
Bibarys <bibaryssultanbek2002@gmail.com>
2023-07-16 16:54:11 +0600

    1.2. Входные данные в тесте [#297750  [#297750 #412583]]

---
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -0,0 +1,50 @@
+package ru.job4j.condition;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import ru.job4j.converter.Converter;
+
+class X2Test {
+
+    @Test
+    void whenA10B0C0X2Then40() {
+        int expected = 40;
+        int rsl = X2.calc(10, 0, 0, 2);
+        Assert.assertEquals(expected, rsl);
+
+    }
+
+    @Test
+    void whenA1B1C1X1Then3() {
+        int expected = 3;
+        int rsl = X2.calc(1, 1, 1, 1);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    void whenA1B1C0X1Then1() {
+        int expected = 2;
+        int rsl = X2.calc(1, 1, 0, 1);
+        Assert.assertEquals(expected, rsl);
+
+    }
+
+    @Test
+    void whenA0B1C1X0Then1() {
+        int expected = 1;
+        int rsl = X2.calc(0, 1, 1, 0);
+        Assert.assertEquals(expected, rsl);
+
+    }
+
+    @Test
+    void whenA0B1C1X1Then2() {
+        int expected = 2;
+        int rsl = X2.calc(0, 1, 1, 1);
+        Assert.assertEquals(expected, rsl);
+
+    }
+
+}
