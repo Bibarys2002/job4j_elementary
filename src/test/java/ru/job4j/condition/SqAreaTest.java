f45c5eeab07941bba7734213119935bae4f85765
Bibarys <bibaryssultanbek2002@gmail.com>
2023-07-21 01:58:52 +0600

    4.5. Отладка программы в IDEA [#159464  [#159464 #413381]]

--- src/test/java/ru/job4j/condition/SqAreaTest.java
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -1,34 +1,25 @@
 package ru.job4j.condition;
 
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
 
- public class SqAreaTest {
-
+class SqAreaTest {
     @Test
-     public void whenP2K3Square2() {
-        double expected = 0.19;
-        int p = 2;
-        double k = 3;
+    void whenP6K2Square2() {
+        int expected = 2;
+        int p = 6;
+        int k = 2;
         double out = SqArea.square(p, k);
-        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+        assertThat(out).isCloseTo(expected, offset(0.01));
     }
 
     @Test
-    public void whenP4K1Square1() {
+    void whenP4K1Square1() {
         int expected = 1;
         int p = 4;
-        double k = 1;
+        int k = 1;
         double out = SqArea.square(p, k);
-        assertThat(out).isEqualTo(expected, withPrecision(0.01));
-    }
-
-    @Test
-    public void whenP3K3Square0dot42() {
-        double expected = 0.42;
-        int p = 3;
-        double k = 3;
-        double out = SqArea.square(p, k);
-        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+        assertThat(out).isCloseTo(expected, offset(0.01));
     }
 }
