1211bdabb7f77d3a2a834e9c245b9c86caf57461
Bibarys <bibaryssultanbek2002@gmail.com>
2024-01-02 19:50:36 +0600

    6.1. Генерация конструкций в IDEA [#279134  [#279134 #413926]]

---
+++ src/main/java/ru/job4j/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,7 @@
+package ru.job4j.ru.job4j.array;
+
+public class ArrayDefinition {
+    Short[] ages = new Short[5];
+    String[] surnames = new String[100500];
+    float[] prices = new float[40];
+}
